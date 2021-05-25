510ad7b8b390314d94d848e5f16434a76686f741
innkuika <innku.ika@gmail.com>
2021-05-24 22:03:21 -0700

    finished

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let options = [];
     options = await getValidSchools()
     // listen for requests :)
     const listener = app.listen(4000, () => {
-        console.log("The static server is listening on port " + listener.address().port);
+        console.log("Ready to serve! The server is listening on port " + listener.address().port);
     });
 })()
 
@@ -25,7 +25,7 @@ app.get("/query/getOptions", function (request, response, next) {
 });
 
 app.get("/query/getPrice", async function (request, response, next) {
-    console.log("sending price");
+    console.log("sending price details");
     const {id} = request.query
     console.log("id ", id)
     let prices = await getPrices(id)
@@ -45,17 +45,18 @@ function timer(ms) {
 
 async function getPrices(id) {
     // const CSAPIKEY = process.env['CSAPIKEY']
-    const CSAPIKEY = 'ze2yK4cctL8zzgnOXmBThvaXJda7wYfB44UWd4Vo';
+    const CSAPIKEY = 'G0Jgcw4uUqyURIXnCHCwQnKeO1Tv5pcLYLUb1Whx';
+    // const CSAPIKEY = 'ze2yK4cctL8zzgnOXmBThvaXJda7wYfB44UWd4Vo';
 
     while (true) {
-        let res = await fetch(`https://api.data.gov/ed/collegescorecard/v1/schools.json?api_key=${CSAPIKEY}&id=${id}&fields=school.name,2018.cost.net_price.consumer.by_income_level.0-30000,2018.cost.net_price.consumer.by_income_level.30001-48000,2018.cost.net_price.consumer.by_income_level.48001-75000,2018.cost.net_price.consumer.by_income_level.75000-plus,2018.cost.net_price.consumer.by_income_level.110001-plus,2018.cost.tuition.out_of_state,2018.cost.attendance.academic_year&page=0`)
+        let res = await fetch(`https://api.data.gov/ed/collegescorecard/v1/schools.json?api_key=${CSAPIKEY}&id=${id}&fields=school.name,2018.cost.net_price.consumer.by_income_level.0-30000,2018.cost.net_price.consumer.by_income_level.30001-48000,2018.cost.net_price.consumer.by_income_level.48001-75000,2018.cost.net_price.consumer.by_income_level.75000-plus,2018.cost.net_price.consumer.by_income_level.110001-plus,2018.cost.tuition.out_of_state,2018.cost.attendance.academic_year,school.city,school.state&page=0`)
         if (!res.ok) {
             console.log(res);
             await timer(3000);
         } else {
             let json = await res.json();
             let result = json['results'];
-            if(result.length === 0) {
+            if (result.length === 0) {
                 continue
             }
             console.log(result)
@@ -68,10 +69,21 @@ async function getPrices(id) {
                 '2018.cost.tuition.out_of_state': tuition,
                 '2018.cost.attendance.academic_year': total_price,
                 'school.name': school_name,
+                'school.city': school_city,
+                'school.state': school_state,
             } = result[0]
 
             return {
-                income_30k, income_48k, income_75k, income_110k, income_110k_plus, tuition, total_price, school_name
+                income_30k,
+                income_48k,
+                income_75k,
+                income_110k,
+                income_110k_plus,
+                tuition,
+                total_price,
+                school_name,
+                school_city,
+                school_state
             }
         }
     }
@@ -79,12 +91,14 @@ async function getPrices(id) {
 
 async function getValidSchools() {
     // const CSAPIKEY = process.env['CSAPIKEY']
-    const CSAPIKEY = 'ze2yK4cctL8zzgnOXmBThvaXJda7wYfB44UWd4Vo';
+    const CSAPIKEY = 'G0Jgcw4uUqyURIXnCHCwQnKeO1Tv5pcLYLUb1Whx';
+    // const CSAPIKEY = 'ze2yK4cctL8zzgnOXmBThvaXJda7wYfB44UWd4Vo';
+
     let page = 0;
 
     let validSchools = [] // [{name:schoolname, value=id}]
     while (true) {
-        let res = await fetch(`https://api.data.gov/ed/collegescorecard/v1/schools.json?api_key=${CSAPIKEY}&school.state=CA&fields=school.name,2018.cost.net_price.consumer.by_income_level.0-30000,2018.cost.net_price.consumer.by_income_level.30001-48000,2018.cost.net_price.consumer.by_income_level.48001-75000,2018.cost.net_price.consumer.by_income_level.75000-plus,2018.cost.net_price.consumer.by_income_level.110001-plus,id,2018.cost.tuition.out_of_state,2018.cost.attendance.academic_year&per_page=100&page=${page}`)
+        let res = await fetch(`https://api.data.gov/ed/collegescorecard/v1/schools.json?api_key=${CSAPIKEY}&school.state=CA&fields=school.name,2018.cost.net_price.consumer.by_income_level.0-30000,2018.cost.net_price.consumer.by_income_level.30001-48000,2018.cost.net_price.consumer.by_income_level.48001-75000,2018.cost.net_price.consumer.by_income_level.75000-plus,2018.cost.net_price.consumer.by_income_level.110001-plus,id,2018.cost.tuition.out_of_state,2018.cost.attendance.academic_year,school.city,school.state&per_page=100&page=${page}`)
 
         if (!res.ok) {
             console.log(res);
@@ -109,10 +123,12 @@ async function getValidSchools() {
                 '2018.cost.tuition.out_of_state': tuition,
                 '2018.cost.attendance.academic_year': total_price,
                 'school.name': school_name,
+                'school.city': school_city,
+                'school.state': school_state,
                 id: id
             } = result[i]
             // check if data is valid
-            if (income_30k && income_48k && income_75k && income_110k && income_110k_plus && tuition && total_price) {
+            if (income_30k && income_48k && income_75k && income_110k && income_110k_plus && tuition && total_price && school_city && school_state) {
                 validSchools.push({
                     name: school_name,
                     value: id
